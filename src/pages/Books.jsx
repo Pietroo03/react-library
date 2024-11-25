2a2d2a615d1ab4a364489fb95d6d1c2e8bbc48da
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-25 10:14:32 +0100

    first step live coding

---
+++ src/pages/Books.jsx
@@ -0,0 +1,27 @@
+export default function () {
+    return (
+        <>
+            <header>
+                <div className="logo">
+                    LOGO
+                </div>
+                <nav>
+                    <a href="">Home</a>
+                    <a href="/books">Books</a>
+                    <a href="/about">About</a>
+                    <a href="/contacts">Contacts</a>
+                </nav>
+            </header>
+            <main>
+                <h1>Books Page</h1>
+            </main>
+            <footer>
+                <div>
+                    Copyright 2024
+                </div>
+            </footer>
+
+        </>
+
+    )
+}
